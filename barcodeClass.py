a79616126bdf1bd6b2acb817f5db0d184296235e
Kostya Vakarian <vakarianplay@gmail.com>
2023-03-21 12:19:57 +0300

    Update barcodeClass.py

--- barcodeClass.py
+++ barcodeClass.py
@@ -2,7 +2,6 @@ from pyzbar import pyzbar
 import cv2
 import os
 
-
 def decode(image):
     global recognizeType
     global recognizeData
@@ -20,8 +19,7 @@ def decode(image):
             recognizeType.append(obj.type)
         return image
     else:
-		#no data block
-        print("nodata")
+	#no data block
         recognizeType = ""
         recognizeData = ""
 
@@ -30,14 +28,12 @@ def draw_barcode(decoded, image):
     # n_points = len(decoded.polygon)
     # for i in range(n_points):
     #     image = cv2.line(image, decoded.polygon[i], decoded.polygon[(i+1) % n_points], color=(0, 255, 0), thickness=5)
-
     image = cv2.rectangle(image, (decoded.rect.left, decoded.rect.top),
                             (decoded.rect.left + decoded.rect.width, decoded.rect.top + decoded.rect.height),
                             color=(0, 255, 0),
                             thickness=5)
     dir = os.path.abspath(os.curdir)
     cv2.imwrite(dir + "/barcode_detected.png", image)
-
     return image
 
 def getData():
